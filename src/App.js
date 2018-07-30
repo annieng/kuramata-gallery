4dc3b5c3753604984a58579c954fb5953ec6454f
Annie Ng <annieng1991@gmail.com>
2018-07-29 19:07:56 -0700

    init

--- src/App.js
+++ src/App.js
@@ -3,16 +3,73 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    // idea is to display only a certain number of components at a time
+    // after determining how many components total will need to be displayed
+    // will loop through the array
+
+    this.state = {
+      cards: [],
+      imageIndex: 1,
+      imageTotal: 0,
+      isLoading: true,
+    }
+  }
+
+  componentDidMount() {
+    this.fetchCards()
+  }
+  // fetch data 
+  async fetchCards() {
+    let res = await fetch('https://api.myjson.com/bins/vz2ze')
+    let data = await res.json()
+    console.log(data)
+    this.setState({
+      cards: data.data,
+      isLoading: true,
+      imageTotal: data.data.length 
+    }),
+      this.setState({
+        isLoading: false
+      })
+    console.log(this.state.cards)
+    console.log(this.state.imageIndex)
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.imageIndex !== prevState.imageIndex) {
+      console.log(this.state.imageIndex + 'component update to this imageIndex')
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='wrapper'>
+        <div className='index-wrap'>
+          <h5 id='index'> {this.state.imageIndex} / {this.state.imageTotal}</h5>
+        </div>
+        <div className='header-wrap'>
+          <div id='title'>
+            <h1> SHIRO KURAMATA </h1>
+          </div>
+        </div>
+
+        <div className='gallery-wrap'>
+
+          { this.state.cards.map( (e,i) => <div key={e.id} data-target={i}
+            ref={ card => this.state.cards.push(card) }
+            id='card-el'>
+            {i+1}
+            </div>)}
+
+          
+        </div>
+
+        <div className='info-wrap'>
+        </div>
+
+        <div className='button-wrap'>
+        </div>
       </div>
     );
   }
